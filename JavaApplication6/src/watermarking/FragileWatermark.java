5cd2bcedd3b8d81edaf5b721f8c5a5f2146800de
zulvaf <zz_vha91@yahoo.com>
2016-10-04 16:26:00 +0700

    Add LSB

--- JavaApplication6/src/watermarking/FragileWatermark.java
+++ JavaApplication6/src/watermarking/FragileWatermark.java
@@ -5,7 +5,6 @@
  */
 package watermarking;
 
-import java.nio.charset.StandardCharsets;
 import vigenere.ExtendedVigenere;
 import java.util.Arrays;
 import java.util.Random;
@@ -32,18 +31,20 @@ public class FragileWatermark {
         generatePseudoRandom();
     }
     
-    public int[] encryptWatermark(){
+    public void putWatermark(){
         String str = binaryToString(watermarkBits);     
         ExtendedVigenere vigenere = new ExtendedVigenere(str, key);
-        return stringToBinary(vigenere.encrypt());
+        changeLSB(stringToBinary(vigenere.encrypt()));
     }
     
     public void generatePseudoRandom(){
+        //Create seed
         int seed = 0;
         for(int i=0; i < key.length(); i++) {
             seed += (int)key.charAt(i) - 'A';
         }
         
+        //Generate sequence of random position
         Random pseudoRandom = new Random(seed);
         for(int i=0; i<watermarkBits.length; i++){
            int random = pseudoRandom.nextInt(watermarkBits.length);
@@ -56,13 +57,14 @@ public class FragileWatermark {
       
     public String binaryToString(int[] bits) {
         
+        //Convert bits in array of int to string
         String stringBits = "";
         for(int i=0; i< bits.length; i++) {
             stringBits += String.valueOf(bits[i]);
         }
-             
-        StringBuilder str = new StringBuilder(stringBits.length()/8);
-               
+        
+        //Generate ascii string based on binary sequence
+        StringBuilder str = new StringBuilder(stringBits.length()/8);      
         for(int i=0; i<stringBits.length(); i++){
             int charCode = Integer.parseInt(stringBits.substring(i,i+8),2);
             str.append((char)charCode);
@@ -73,26 +75,38 @@ public class FragileWatermark {
     }
     
     public int[] stringToBinary(String str) {
-        int[] bytes = new int[str.length()];
         
+        //Generate bytes of char in string 
+        int[] bytes = new int[str.length()];       
         for(int i=0; i< str.length(); i++) {
             bytes[i] = (int)str.charAt(i);
         }
         
-        int[] binary = new int[str.length() * 8];
-        StringBuilder builder = new StringBuilder(str.length()*8);
-        
-        
+        //Create string of binary sequence from bytes
+        StringBuilder builder = new StringBuilder(str.length()*8);             
         for(int i=0; i< bytes.length; i++) {
             String bin = Integer.toBinaryString(bytes[i]);
             String format = String.format("%8s", bin).replace(' ', '0');
             builder.append(format);
         }
         
+        //Move string to array of integer
+        int[] binary = new int[str.length() * 8];
         for(int i=0; i < builder.length(); i++) {
             binary[i] = Integer.parseInt(builder.substring(i,i+1));
         }
         
         return binary;
+    }
+    
+    public void changeLSB(int[] watermark){      
+        for(int i=0; i < watermark.length; i++) {
+            int index = locations[i] + 23;
+            steganoBits[index] = watermark[i];
+        }
+    }
+    
+    public int[] getStegano(){
+        return steganoBits;
     }
 }
--- JavaApplication6/src/watermarking/Main.java
+++ JavaApplication6/src/watermarking/Main.java
@@ -14,26 +14,17 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) throws UnsupportedEncodingException {
-        int [] w = {0,1,1,1,1,0,1,0,0,1,1,1,0,1,0,1,0,1,1,0,1,1,0,0,0,1,1,1,0,1,1,0,0,1,1,0,0,0,0,1};
-        int [] f = {1};
-        int [] result;
-
-        String key = "stegano";
-        FragileWatermark test = new FragileWatermark(w, f, key);
-        result = test.encryptWatermark();
-        for(int i=0; i< result.length; i++) {
-            System.out.print(result[i]);
-        }
-        System.out.println();
         
         // Preprocess watermark file
-//        Scanner input = new Scanner(System.in);
-//        System.out.print("Enter filename for watermark (BMP/PNG): ");
-//        String filename = input.nextLine();
-//        WatermarkFile watermark = new WatermarkFile("waee.png");
-//        
-//        watermark.getBits();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter filename for watermark (BMP/PNG): ");
+        String filename = input.nextLine();
+        System.out.print("Enter filename for cover image (BMP/PNG): ");
+        filename = input.nextLine();
+        WatermarkFile watermark = new WatermarkFile(filename);
+       
+        int[] watermarkBits = watermark.getBits();
         
-        
+      
     }
 }
